94195b8bf685c104452c8ac6a1f26fbff375645d
wangzp <6010625@qq.com>
2020-04-10 16:25:27 +0800

    手写call

--- call.js
+++ call.js
@@ -2,13 +2,20 @@
  * 手写call
  */
 Function.prototype.myCall = function() {
-    var _this = arguments[0] || Window;     // 获取this指向的目标
-    var args = [];      // 存放实际参数
+    // 获取this指向的目标
+    var _this = arguments[0] || Window;
+    // 存放实际参数
+    var args = [];
     for (var i=1, len=arguments.length; i<len; i++) {
         args.push(arguments[i]);
     }
-    _this.fn = this;    // 把自身绑定到目标上
-    var res = eval("_this.fn("+ args.join(",") +")");  //   用eval调用，否则实参买法传，并接收返回结果
-    delete _this.fn;    // 过河拆桥
+
+    // 把自身绑定到目标上
+    _this.fn = this;
+    // 用eval调用，否则实参买法传，并接收返回结果
+    var res = eval("_this.fn("+ args.join(",") +")");
+    // 删除多余的fn
+    delete _this.fn;
+    // 返回执行结果
     return res;
 }
